5f107f1a83dcbe4ecd349d9e76ba96f76d7997b1
Lori White <lori.r.white.24@gmail.com>
2021-11-05 11:50:59 -0400

    Updated creating_tables_inserting_data_and_importing_data.sql

--- creating_tables_inserting_data_and_importing_data.sql
+++ creating_tables_inserting_data_and_importing_data.sql
@@ -27,3 +27,40 @@ employee_id INT NOT NULL AUTO_INCREMENT;
 # used to drop a table and the next line is to drop a database
 -- DROP TABLE book_store.employees;
 -- DROP DATABASE book_store;
+
+# Inserting Data
+# Adds a single record
+INSERT INTO book_store.employees 
+VALUES(1,"Charlie",'P',TRUE,20.00);
+# Adds multiple records
+INSERT INTO book_store.employees
+VALUES(2,"Vera",'N',FALSE,0.00),
+	  (3,"Lisa",'P',TRUE,20.50);
+# Adding with listing the columns
+INSERT INTO book_store.employees (employee_name, employee_type)
+VALUES("John",'P');
+INSERT INTO book_store.employees (employee_name, employee_type, was_paid, money_due)
+VALUES("Bob",'N',false,50.00);
+
+# Describe Statement
+DESCRIBE book_store.employees;
+# Show Statement
+SHOW DATABASES;
+USE book_store;
+SHOW TABLES;
+
+# Creating the customer table
+CREATE TABLE book_store.loyalty_customers (
+    customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    family_name VARCHAR(25) NOT NULL,
+    given_name VARCHAR(20) NOT NULL,
+    loyalty_number VARCHAR(25) NOT NULL,
+    contact_number VARCHAR(14) NOT NULL,
+    email_address VARCHAR(50) NOT NULL,
+    points_balance INT DEFAULT 0
+);
+
+# Importing a CSV file
+SELECT @@GLOBAL.secure_file_priv;
+load data infile 'c:/ProgramData/MySQL/MySQl Server 8.0/Uploads/loyalty.csv' into table book_store.loyalty_customers
+fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
